10dd95e1db086ae782cfbf033c2c31667fb99ed6
Ben Carter <51202986+ben-cart3r@users.noreply.github.com>
2022-09-03 15:47:57 +0100

    feat: add ability to construct choice state (#5)

---
+++ states/choice/outputs.tf
@@ -0,0 +1,9 @@
+output "definition" {
+  value       = local.definition
+  description = "The definition of the state"
+}
+
+output "name" {
+  value       = var.name
+  description = "Name of the state"
+}

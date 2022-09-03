ef60995a26ba4da46d3fca6f365e89142d2452ad
Ben Carter <51202986+ben-cart3r@users.noreply.github.com>
2022-09-03 15:22:28 +0100

    feat: add ability to construct termination states (#3)

---
+++ states/fail/main.tf
@@ -0,0 +1,9 @@
+locals {
+  definition = merge(
+    {
+      Type = "Fail"
+    },
+    var.cause == null ? null : { Comment = var.cause },
+    var.error == null ? null : { InputPath : var.error },
+  )
+}
---
+++ states/succeed/outputs.tf
@@ -0,0 +1,9 @@
+output "definition" {
+  value       = { Type = "Succeed" }
+  description = "The definition of the state"
+}
+
+output "name" {
+  value       = var.name
+  description = "Name of the state"
+}

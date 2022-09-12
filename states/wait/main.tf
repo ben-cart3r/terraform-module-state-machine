14db4fd299974e69ef071c32360ad93c1a5f075d
Ben Carter <51202986+ben-cart3r@users.noreply.github.com>
2022-09-12 19:06:30 +0100

    bug: fix some typos and mistakes (#9)

--- states/wait/main.tf
+++ states/wait/main.tf
@@ -1,16 +1,16 @@
 locals {
   definition = merge(
     {
-      Type = "Pass"
+      Type = "Wait"
     },
     var.comment == null ? null : { Comment = var.comment },
     var.end ? { End = true } : null,
     var.end ? null : { Next = var.next },
-    var.input_path == null ? null : { InputPath : var.input_path },
-    var.output_path == null ? null : { OutputPath : var.output_path },
-    var.seconds == null ? null : { Parameters : var.seconds },
-    var.seconds_path == null ? null : { Result : var.seconds_path },
-    var.timestamp == null ? null : { ResultPath : var.timestamp },
-    var.timestamp_path == null ? null : { Result : var.timestamp_path },
+    var.input_path == null ? null : { InputPath = var.input_path },
+    var.output_path == null ? null : { OutputPath = var.output_path },
+    var.seconds == null ? null : { Parameters = var.seconds },
+    var.seconds_path == null ? null : { Result = var.seconds_path },
+    var.timestamp == null ? null : { ResultPath = var.timestamp },
+    var.timestamp_path == null ? null : { Result = var.timestamp_path },
   )
 }
